92c782e81fe5f0c83d5b75df7c600d6b110971c7
jrb <>
2012-08-16 23:50:20 +0000

    for Windows no-installed-headers

--- xmlBaseLib.py
+++ xmlBaseLib.py
@@ -1,7 +1,10 @@
-#$Header:$
+#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/xmlBase/xmlBaseLib.py,v 1.4 2009/01/23 00:21:04 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly',0):
         env.Tool('addLibrary', library = ['xmlBase'], package = 'xmlBase')
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'xmlBase') 
+
     env.Tool('facilitiesLib')
     env.Tool('addLibrary', library = env['xercesLibs'])
 
